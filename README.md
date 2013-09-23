01d88b3c5b8fc390b0f665fd0b758b614a24d136
Charles Bueche <bueche@netnea.com>
2013-09-23 16:12:07 +0200

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,50 @@ splashid2kepass
 ===============
 
 convert from SplashID Safe to KeePass Password Safe
+
+Installation
+------------
+
+stdlib's csv module does not support Unicode
+use unicodecsv from Jeremy Dunck
+
+- git clone https://github.com/jdunck/python-unicodecsv.git
+- cd python-unicodecsv
+- sudo python setup.py install
+
+Usage
+-----
+
+in SplashID for Mac :
+
+- File / Export / CSV
+- name your output file export_csv.csv
+- select "all records"
+- save
+- mail : no
+
+python ./splashid_to_keepass.py -i export_csv.csv -o import.csv
+
+open KeePass 2.x on Windows
+
+- File / Import / Generic CSV Importer
+- select import.csv
+- choose the text encoding and check the preview
+- in the structure tab, select
+    - comma as field separator
+    - " as text qualifier
+    - new line as record separator
+    - ignore 1st row
+- in the semantic field list, define the following fields to match import.csv
+    - Title
+    - User Name
+    - Password
+    - URL
+    - last mod time : yyyy'-'MM'-'dd'T'HH':'mm';'ss'
+    - Notes
+    - Group
+- check the preview to see if the import would work as expected
+- go for it
+
+before deleting your old SplashID database, be sure that all the records imported fine.
+at end of process, delete all your temporary export and import files from your disk
---
+++ splashid_to_keepass.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+#
+# splashid_to_keepass.py - convert SplashID export to KeePass import
+#
+# Ch. Bueche <bueche@netnea.com>
+# 23.9.2013 : initial version
+#
+# ------------------------------------------------------------------------------------------
+# installation and usage : https://github.com/cbueche/splashid2kepass
+# ------------------------------------------------------------------------------------------
+
+
+from optparse import OptionParser
+import os.path
+import unicodecsv
+import sys
+import time
+
+
+# -----------------------------------------------------------
+# main
+# -----------------------------------------------------------
+
+parser = OptionParser()
+parser.add_option("-i", "--input-file",  dest  ="input_file", default="", help="input file")
+parser.add_option("-o", "--output-file", dest ="output_file", default="", help="output file")
+(options, args) = parser.parse_args()
+
+if os.path.isfile(options.input_file):
+	pass
+else:
+	print "Cannot find input file"
+	sys.exit(1)
+
+# read exported CSV
+
+entries = []
+with open(options.input_file, 'rU') as csvfile:
+
+	reader = unicodecsv.reader(csvfile, encoding='MACROMAN')
+	try:
+		for row in reader:
+			if row[0] == 'SplashID Export File':
+				continue
+			# print 'DECODED : ', ', '.join(row)
+			entry = {}
+			entry['type']        = row[0]
+			entry['description'] = row[1]
+			entry['username']    = row[2]
+			entry['password']    = row[3]
+			entry['url']         = row[4]
+			entry['f5']          = row[5]
+			entry['f6']          = row[6]
+			entry['f7']          = row[7]
+			entry['f8']          = row[8]
+			entry['f9']          = row[9]
+			entry['modified']    = row[10]
+			entry['notes']       = row[11]
+			entry['category'] = row[12]
+			entries.append(entry)
+	except csv.Error as e:
+		sys.exit('file %s, line %d: %s' % (options.input_file, reader.line_num, e))
+
+# write import CSV for KeePass
+
+with open(options.output_file, 'w') as csvoutput:
+
+	writer = unicodecsv.writer(csvoutput, encoding='utf-8')
+
+	# header
+	writer.writerow(('Description', 'Username', 'Password', 'URL', 'Modified', 'Notes', 'Category'))
+
+	for entry in entries:
+
+		# format date in something easier to import
+		entry['modified'] = time.strftime("%Y-%m-%dT00:00:00", time.strptime(entry['modified'], "%B %d,%Y"))
+
+		# fields 5 to 9 are better at end of notes than in their own attributes
+		for field in ['f5', 'f6', 'f7', 'f8', 'f9']:
+			# print "field %s : <%s>" % (field, entry[field])
+			if entry[field] is None or entry[field] == '' or entry[field] == ' ':
+				pass
+			else:
+				entry['notes'] += "\n"
+				entry['notes'] += entry[field]
+
+		# output the new CSV record
+		writer.writerow((	entry['description'],
+							entry['username'],
+							entry['password'],
+							entry['url'],
+							entry['modified'],
+							entry['notes'],
+							entry['category']
+							))
+
+print "%s records converted" % (len(entries))
+print "conversion done. Be sure to delete your temporary files after successful import into KeePass"
